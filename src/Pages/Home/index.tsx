9a87b00e74230dfe925cb0178a333cd0823e7985
Yasar Cakir <yasar.cakir@mirsis.com.tr>
2023-12-16 22:37:10 +0300

    Refactor Home component layout

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,23 +1,15 @@
 import type { FC } from 'react';
-
 import { useTranslation } from 'react-i18next';
-import { motion } from 'framer-motion';
 
 interface HomeProps {}
 
 export const Home: FC<HomeProps> = () => {
   const { t } = useTranslation('translation', { keyPrefix: 'home' });
   return (
-    <motion.section
-      initial={{ opacity: 0, x: -100 }}
-      animate={{ opacity: 1, x: 0 }}
-      exit={{ opacity: 0, x: -100 }}
-      transition={{ duration: 0.5 }}
-      className="container"
-    >
+    <section>
       <h1>{t('title')}</h1>
       <p>hwdswdc</p>
       <div>fgdfgdfgdf</div>
-    </motion.section>
+    </section>
   );
 };
