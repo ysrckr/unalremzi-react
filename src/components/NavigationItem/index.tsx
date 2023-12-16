7268bbb198816c716603ee29ab1dcc1263d3fecb
Yasar Cakir <yasar.cakir@mirsis.com.tr>
2023-12-16 22:56:58 +0300

    Refactor NavigationItem component and add module styles

--- src/components/NavigationItem/index.tsx
+++ src/components/NavigationItem/index.tsx
@@ -1,13 +1,16 @@
-import { Link } from '@tanstack/react-router';
 import type { FC } from 'react';
-import type { NavigationItem as NavItem } from '../../data/navigationItems';
+import { Link } from '@tanstack/react-router';
+import type { NavigationItem as NavItem } from '@/data/navigationItems';
+import styles from './navigationItem.module.scss';
 
 interface NavigationItemProps extends NavItem {}
 
 export const NavigationItem: FC<NavigationItemProps> = ({ url, label }) => {
   return (
-    <li>
-      <Link to={url}>{label}</Link>
+    <li className={styles.item}>
+      <Link to={url} className={styles.link}>
+        {label}
+      </Link>
     </li>
   );
 };
