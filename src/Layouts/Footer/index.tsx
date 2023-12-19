13a0ded3d19a4095ef85274c129c3e05f035dfba
Yasar Cakir <yasar.cakir@mirsis.com.tr>
2023-12-19 20:44:37 +0300

    Refactor footer and header to use arrow function syntax and add scroll effect

--- src/Layouts/Footer/index.tsx
+++ src/Layouts/Footer/index.tsx
@@ -1,12 +1,14 @@
 import { CompanyLogo } from '@/components/CompanyLogo';
-import type { FC } from 'react';
 import { companyLogo } from '@/utils/constants';
+import type { FC } from 'react';
 import styles from './footer.module.scss';
 
 interface FooterProps {}
 
 export const Footer: FC<FooterProps> = () => {
-  return <footer className={styles.footer}>
-    <CompanyLogo src={companyLogo} />
-  </footer>;
+  return (
+    <footer className={styles.footer}>
+      <CompanyLogo src={companyLogo} />
+    </footer>
+  );
 };
