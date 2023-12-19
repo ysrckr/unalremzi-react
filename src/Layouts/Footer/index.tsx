72cf85b2f01ff25dd4b0439a0c5ccb922445c9ba
Yasar Cakir <yasar.cakir@mirsis.com.tr>
2023-12-19 19:46:09 +0300

    Add footer styling to the Footer component

--- src/Layouts/Footer/index.tsx
+++ src/Layouts/Footer/index.tsx
@@ -1,11 +1,12 @@
 import { CompanyLogo } from '@/components/CompanyLogo';
 import type { FC } from 'react';
 import { companyLogo } from '@/utils/constants';
+import styles from './footer.module.scss';
 
 interface FooterProps {}
 
 export const Footer: FC<FooterProps> = () => {
-  return <footer>
+  return <footer className={styles.footer}>
     <CompanyLogo src={companyLogo} />
   </footer>;
 };
