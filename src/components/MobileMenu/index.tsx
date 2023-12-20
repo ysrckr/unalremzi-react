60a19ba364f73afb944cb8a9cebf25c7bc83b0e9
Yasar Cakir <yasar.cakir@mirsis.com.tr>
2023-12-20 20:54:26 +0300

    Update MobileMenu animation and transition properties

--- src/components/MobileMenu/index.tsx
+++ src/components/MobileMenu/index.tsx
@@ -1,16 +1,16 @@
-import { socialMediaList } from '@/data/socialMediaList';
-import { motion } from 'framer-motion';
 import type { FC } from 'react';
 import { Navigation } from '../Navigation';
 import { SocialMedia } from '../SocialMedia';
+import { motion } from 'framer-motion';
+import { socialMediaList } from '@/data/socialMediaList';
 import styles from './mobileMenu.module.scss';
 
 interface MobileMenuProps {}
 
 export const MobileMenu: FC<MobileMenuProps> = () => {
   const variants = {
-    open: { opacity: 1, x: 0 },
-    closed: { opacity: 0, x: '-100%' },
+    open: { opacity: 1, borderRadius: '0%', x: 0, y: 0 },
+    closed: { opacity: 0, borderRadius: '50%', x: '100%', y: '-100%' },
   };
   return (
     <motion.div
@@ -19,7 +19,7 @@ export const MobileMenu: FC<MobileMenuProps> = () => {
       initial="closed"
       animate="open"
       exit="closed"
-      transition={{ damping: 300 }}
+      transition={{ damping: 300, duration: 0.5 }}
     >
       <Navigation device="mobile" />
       <SocialMedia socialMediaList={socialMediaList} />
