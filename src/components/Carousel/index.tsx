b8311b72bd947f2a0b631c795ed1029ac36c0868
Yasar Cakir <yasar.cakir@mirsis.com.tr>
2023-12-20 21:38:01 +0300

    Refactor Image interface properties

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,5 +1,5 @@
-import { FC } from 'react';
 import type { Image } from '@/types/Images';
+import { FC } from 'react';
 import styles from './carousel.module.scss';
 
 interface CarouselProps {
--- src/types/Images.ts
+++ src/types/Images.ts
@@ -1,4 +1,4 @@
-export interface Image { 
+export interface Image {
   src: string;
   alt: string;
 }
